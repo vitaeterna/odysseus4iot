4e05eb009339123b62aa45311d56d07d2affeff5
Michael Sünkel <vitaeterna@gmx.de>
2022-02-04 06:00:08 +0100

    Added requirements.txt

--- odysseus4iot_eclipse-project/docker/docker-postgres/docker-run-postgres.bat
+++ odysseus4iot_eclipse-project/docker/docker-postgres/docker-run-postgres.bat
@@ -1,2 +1,2 @@
-docker run -p 5555:5432 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -e PGDATA=/postgres -it postgres:12.9
+docker run -p 5432:5432 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -e PGDATA=/postgres -it msuenkel/percom2022-postgres:latest
 pause
